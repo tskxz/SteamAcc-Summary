53396ffc6971acf52e8c5de933205f180c1e4f21
Tanjil <tanjilkh@gmail.com>
2025-06-10 14:52:39 +0100

    chore: delete repetitive names

--- scripts/get_name_games.py
+++ scripts/get_name_games.py
@@ -3,5 +3,9 @@ import json
 with open('owned_games.json', 'r', encoding='utf-8') as f:
     games = json.load(f)
 
+seen = set()
 for game in games:
-    print(game['name'])
+    name = game['name']
+    if name not in seen:
+        print(name)
+        seen.add(name)
